9d1a4fdd67b5887adfb349fe538ed75a89479090
The-Earth <26201356+The-Earth@users.noreply.github.com>
2023-05-12 10:05:05 -0400

    [Add](main.py) Add target time detection

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import json
+import re
+from datetime import datetime
 import mwclient
 
 config = json.load(open('config.json', 'r', encoding='utf-8'))
 site = mwclient.Site(config['site'])
 site.login(config['bot_username'], config['bot_password'])
 template_name = 'Template:TalkpageCountdown'
+template_regex = re.compile(r'\{\{TalkpageCountdown\s*?\|(.+?\||)target-time=([\d\-:TZ+]{8,25}).*?\}\}')
 log_page_name = 'User:Tiger-bot/watchlist/1'
 
 
@@ -45,6 +48,7 @@ def get_sections_with_template(page_title: str) -> list[tuple[str, str]]:
     :param page_title: The title to work on
     :return: List of top level sections containing the template
     """
+    # Get list of all sections
     query_sections_data = {
         "format": "json",
         "page": page_title,
@@ -52,16 +56,16 @@ def get_sections_with_template(page_title: str) -> list[tuple[str, str]]:
         "utf8": 1,
         "formatversion": "2"
     }
-
     section_result = site.api('parse', **query_sections_data)
     sections = section_result['parse']['sections']
     section_idx = [item['index'] for item in sections if item['fromtitle'] == page_title and item['toclevel'] == 1]
 
-    section_with_template = []
+    # From each section, find if there's target template in it
+    section_with_template: list[tuple[str, str]] = []   # (section title, target time)
     query_section_template_data = {
         "format": "json",
         "page": page_title,
-        "prop": "templates",
+        "prop": "templates|wikitext|sections",
         # "section": "16",
         "utf8": 1,
         "formatversion": "2"
@@ -71,29 +75,35 @@ def get_sections_with_template(page_title: str) -> list[tuple[str, str]]:
         template_result = site.api('parse', **query_section_template_data)
         for template in template_result['parse']['templates']:
             if template['title'] == template_name:
-                section_with_template.append(idx)
+                # Target template found
+                template_search = template_regex.search(template_result['parse']['wikitext'])
+                target_time = template_search.groups()[1]
+                # (section index, section title, target time)
+                section_with_template.append((template_result['parse']['sections']['line'], target_time))
 
-    output: list[tuple[str, str]] = []      # (section index, section title)
-    for section in sections:
-        if section['index'] in section_with_template:
-            output.append((section['index'], section['line']))
-    return output
+    return section_with_template
 
 
 def main():
     page_with_template = get_transclude_in()
-    sections_with_template: dict[str, list[tuple[str, str]]] = {}
+    sections_with_template: dict[str, list[tuple[str, str]]] = {}   # {page title: (section title, target time)}
 
-    log_text = ''
+    unexpired_log_text = '== 未到期 ==\n'
+    expired_log_text = '== 已到期 ==\n'
 
     for page_title in page_with_template:
-        sections = get_sections_with_template(page_title)   # (section index, section title
+        sections = get_sections_with_template(page_title)   # (section title, target time)
         sections_with_template[page_title] = sections
         for section in sections:
-            log_text += f'* [[{page_title}#{section[1]}]]\n'
+            if datetime.utcnow() < datetime.fromisoformat(section[1]):
+                unexpired_log_text += f'* [[{page_title}#{section[0]}]] - {section[1]}\n'
+            else:
+                expired_log_text +=f'* [[{page_title}#{section[0]}]] - {section[1]}\n'
+
+    log_text = f'{expired_log_text}\n\n{unexpired_log_text}\n'
 
     log_page = site.pages[log_page_name]
-    log_page.edit(log_text, '')
+    log_page.edit(log_text, f'更新{template_name}使用情况')
 
 
 if __name__ == '__main__':
